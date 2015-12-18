c010402637c55af0eef4236c71d746bb6c06d8df
iLexiconn <ilexiconn@gmail.com>
2015-12-18 22:43:49 +0100

    Released 0.1.3 Fixed startup crash

--- src/main/java/net/ilexiconn/lifegauge/server/asm/LifeGaugeTransformer.java
+++ src/main/java/net/ilexiconn/lifegauge/server/asm/LifeGaugeTransformer.java
@@ -1,6 +1,5 @@
 package net.ilexiconn.lifegauge.server.asm;
 
-import cpw.mods.fml.common.FMLLog;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
@@ -36,37 +35,39 @@ public class LifeGaugeTransformer implements IClassTransformer {
         String potionEffectClass = LifeGaugePlugin.isDeobfuscated ? "rw" : "net/minecraft/potion/PotionEffect";
 
         String onNewPotionEffect = LifeGaugePlugin.isDeobfuscated ? "a" : "onNewPotionEffect";
+        String onNewPotionEffectDeobf = "onNewPotionEffect";
         String onNewPotionEffectDesc = String.format("(L%s;)V", potionEffectClass);
 
         String onChangedPotionEffect = LifeGaugePlugin.isDeobfuscated ? "a" : "onChangedPotionEffect";
+        String onChangedPotionEffectDeobf = "onChangedPotionEffect";
         String onChangedPotionEffectDesc = String.format("(L%s;Z)V", potionEffectClass);
 
         String onFinishedPotionEffect = LifeGaugePlugin.isDeobfuscated ? "b" : "onFinishedPotionEffect";
+        String onFinishedPotionEffectDeobf = "onFinishedPotionEffect";
         String onFinishedPotionEffectDesc = String.format("(L%s;)V", potionEffectClass);
 
         for (MethodNode method : classNode.methods) {
             if (onNewPotionEffect.equals(method.name) && onNewPotionEffectDesc.equals(method.desc)) {
-                transform(method, onNewPotionEffect, onNewPotionEffectDesc, potionEffectClass, entityLivingBaseClass);
+                transform(method, onNewPotionEffectDeobf, potionEffectClass, entityLivingBaseClass);
             } else if (onChangedPotionEffect.equals(method.name) && onChangedPotionEffectDesc.equals(method.desc)) {
-                transform(method, onChangedPotionEffect, onChangedPotionEffectDesc, potionEffectClass, entityLivingBaseClass);
+                transform(method, onChangedPotionEffectDeobf, potionEffectClass, entityLivingBaseClass);
             } else if (onFinishedPotionEffect.equals(method.name) && onFinishedPotionEffectDesc.equals(method.desc)) {
-                transform(method, onFinishedPotionEffect, onFinishedPotionEffectDesc, potionEffectClass, entityLivingBaseClass);
+                transform(method, onFinishedPotionEffectDeobf, potionEffectClass, entityLivingBaseClass);
             }
         }
 
         return createByteArrayFromClass(classNode, ClassWriter.COMPUTE_MAXS);
     }
 
-    public void transform(MethodNode methodNode, String name, String desc, String var1, String var2) {
-        FMLLog.info("Transforming " + name + desc + "...");
+    public void transform(MethodNode methodNode, String name, String potionEffectClass, String entityLivingBaseClass) {
         for (int i = 0; i < methodNode.instructions.size(); i++) {
             AbstractInsnNode insnNode = methodNode.instructions.get(i);
             if (insnNode.getOpcode() == Opcodes.GETSTATIC) {
-                InsnList invoc = new InsnList();
-                invoc.add(new VarInsnNode(Opcodes.ALOAD, 1));
-                invoc.add(new VarInsnNode(Opcodes.ALOAD, 0));
-                invoc.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "net/ilexiconn/lifegauge/server/asm/LifeGaugeHooks", name, String.format("(L%s;L%s;)V", var1, var2), false));
-                methodNode.instructions.insertBefore(insnNode, invoc);
+                InsnList insnList = new InsnList();
+                insnList.add(new VarInsnNode(Opcodes.ALOAD, 1));
+                insnList.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                insnList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "net/ilexiconn/lifegauge/server/asm/LifeGaugeHooks", name, String.format("(L%s;L%s;)V", potionEffectClass, entityLivingBaseClass), false));
+                methodNode.instructions.insertBefore(insnNode, insnList);
                 break;
             }
         }
